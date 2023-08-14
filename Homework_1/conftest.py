8a4f624c56410840701de81c00769d8fee2f97ee
Ustinova <a.s.ustinowa@gmail.com>
2023-08-14 23:53:19 +0400

    Homework_1 update

---
+++ Homework_1/conftest.py
@@ -0,0 +1,36 @@
+import json
+import pytest
+import requests
+import yaml
+
+with open("config.yaml", "r") as f:
+    d = yaml.safe_load(f)
+
+
+def auth():
+    data = {
+        "username": d["username"],
+        "password": d["password"]
+    }
+    res = requests.post(url=d["url_auth"], data=json.dumps(data))
+    return res.json()["token"]
+
+
+@pytest.fixture()
+def create_post():
+    token = auth()
+    headers = {
+        "X-Auth-Token": token
+    }
+    data = {
+        "title": "my_title",
+        "description": "my_description",
+        "content": "my_content",
+    }
+    res = requests.post(url=d["url_create"], headers=headers, data=json.dumps(data))
+    # print(res.content)
+    return res.json()["item"]["description"]
+
+
+# print(create_post())
+# print(auth())
--- Homework_1/test_post.py
+++ Homework_1/test_post.py
@@ -1,5 +1,3 @@
-from create_post import create_post
 
-
-def test_step1():
-    assert create_post() == "my_description", "Test"
+def test_step1(create_post):
+    assert create_post == "my_description", "Test"
